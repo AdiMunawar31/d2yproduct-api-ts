3fc26bc6e47ebab99fd1c8d5f33598db3601d94d
Adi Munawar <munawar.adi31@gmail.com>
2022-05-17 10:13:10 +0700

    add config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+import config from 'config';
 
 const app = express()
 
-const PORT = 4000;
+const PORT = config.get<string>('port');
 app.listen(PORT, () => console.log(`🔥Server Listening in http://localhost:${PORT}`));
