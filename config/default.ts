55b903e18ffe560d6d5cf1095f6096b7b988eb6e
Adi Munawar <munawar.adi31@gmail.com>
2022-05-19 07:14:34 +0700

    validateResource middleware

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
     port: 5000,
-    dbUri: 'mongodb+srv://ulin:2qbEQDsvQNpkAtJM@cluster0.mnfc0.mongodb.net/d2yshop-api?retryWrites=true&w=majority'
+    dbUri: 'mongodb+srv://ulin:2qbEQDsvQNpkAtJM@cluster0.mnfc0.mongodb.net/d2yshop-api?retryWrites=true&w=majority',
+    saltWorkFactor: 10,
 }
